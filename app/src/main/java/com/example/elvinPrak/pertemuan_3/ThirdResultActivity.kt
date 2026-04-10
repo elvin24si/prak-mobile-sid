ccebc830e9bbb6188c674b0a435333b2417ef7f4
elvin24si <elvin24si@mahasiswa.pcr.ac.id>
2026-04-10 10:32:56 +0700

    Pertemuan 3: Penerapan attribut dan intent

--- app/src/main/java/com/example/elvinPrak/pertemuan_3/ThirdResultActivity.kt
+++ app/src/main/java/com/example/elvinPrak/pertemuan_3/ThirdResultActivity.kt
@@ -1,37 +1,21 @@
 package com.example.elvinPrak.pertemuan_3
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.elvinPrak.R
-import com.example.elvinPrak.databinding.ActivityThirdBinding
 
-class ThirdActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityThirdBinding
+class ThirdResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityThirdBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_third_result)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val inputPhone: EditText = findViewById(R.id.inputNoTujuan)
-        val btnKirim: Button = findViewById(R.id.btnKirim)
-
-        binding.btnKirim.setOnClickListener {
-            val intent = Intent(this, ThirdResultActivity::class.java)
-            startActivity(intent)
         }
     }
 }

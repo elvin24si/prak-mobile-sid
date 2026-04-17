24e4666b2b33b00781a5d24b7026a77bc9be10ee
elvin24si <elvin24si@mahasiswa.pcr.ac.id>
2026-04-17 07:58:22 +0700

    Pertemuan 4: warming up

--- app/src/main/java/com/example/elvinPrak/pertemuan_4/FourthActivity.kt
+++ app/src/main/java/com/example/elvinPrak/pertemuan_4/FourthActivity.kt
@@ -1,32 +1,28 @@
-package com.example.elvinPrak
+package com.example.elvinPrak.pertemuan_4
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.elvinPrak.databinding.ActivityMainBinding
-import com.example.elvinPrak.pertemuan_4.FourthActivity
+import com.example.elvinPrak.R
+import com.example.elvinPrak.pertemuan_3.ThirdResultActivity
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class FourthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_fourth)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding.btn.setOnClickListener {
-            val intent = Intent(this, FourthActivity::class.java)
-            intent.putExtra("name", "Politeknik Caltex Riau")
-            intent.putExtra("from", "Rumbai")
-            intent.putExtra("age", 25)
-            startActivity(intent)
-        }
+        val name = intent.getStringExtra("name")
+        val from = intent.getStringExtra("from")
+        val age = intent.getIntExtra("age",0)
+        Log.e("Data Intent","Nama: $name , Usia: $age, Asal: $from")
     }
 }
